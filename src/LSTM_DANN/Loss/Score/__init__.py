df38c9da1b1b507c20702d7e41995f794b55e9ee
Yasantha Niroshan <niroshanyi@gmail.com>
2026-08-26 06:19:32 +0530

    Added LSTM-DANN implementation - Added LSTM-DANN Basic Implementation - Added Model, Loss function, Trainer and Tester - Added Reporter setup with wandb

---
+++ src/LSTM_DANN/Loss/Score/__init__.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class Score(nn.Module):
+
+    def __init__(self, a_1: float, a_2: float):
+        super().__init__()
+        self.a1 = a_1
+        self.a2 = a_2
+
+    def _calculate_score(self, residue):
+        """
+        Calculate the score based on the residue.
+        Equation : e^(-residue/a1) - 1 if residue < 0 else e^(residue/a2) - 1
+        """
+        value = torch.where(
+            residue < 0,
+            torch.exp(-residue / self.a1),
+            torch.exp(residue / self.a2),
+        )
+        return value - 1
+
+    def forward(self, y_pred, y_true):
+        residue = y_pred - y_true
+        score = self._calculate_score(residue)
+        return torch.sum(score)
---
+++ src/LSTM_DANN/Loss/__init__.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class RegressionLoss(nn.Module):
+
+    def __init__(self,p):
+        super(RegressionLoss, self).__init__()
+        self.p = p
+
+    def forward(self, y_pred, y_true):
+        return torch.mean(torch.abs(y_pred - y_true) ** self.p)
+
+
+class ClassificationLoss(nn.Module):
+
+    def __init__(self):
+        super(ClassificationLoss, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, y_pred, y_true):
+        y_pred = self.sigmoid(y_pred)
+        return torch.mean(-y_true * torch.log(y_pred) - (1 - y_true) * torch.log(1 - y_pred))
